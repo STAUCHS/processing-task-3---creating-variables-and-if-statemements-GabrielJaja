817c79e9b9db6cfdcb62df04c4ce0795586498e9
GabrielJaja <gabriel.jaja25@ycdsbk12.ca>
2024-03-07 14:05:57 -0500

    5.2 Task for Randomizing the colour

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,68 @@
 import processing.core.PApplet;
 
+import java.util.Random;
+
 public class Sketch extends PApplet {
+	int intRed;
+  int intBlue;
+  int intGreen;
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
 	// put your size call here
     size(400, 400);
+
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    
+    // making Random int
+    Random myRandom = new Random();
+    intRed = myRandom.nextInt(0, 255);
+    intBlue = myRandom.nextInt(0, 255);
+    intGreen = myRandom.nextInt(0, 255);
+  
+
+    background(myRandom.nextInt(0, 255), myRandom.nextInt(0, 255), myRandom.nextInt(0, 255));
+    text("More Colours", 40, 333);
+    
+    // Time Display
+    int hr = hour();
+    int min = minute();
+    int year = year();
+    text(min, 300, 160);
+    text(hr, 200, 110);
+    text(year, 400, 400);
+    
+    if (intBlue < 90) {
+      fill(intBlue, intGreen, intRed);
+    }
+    if (minute() < 10) {
+    }
+    else if (intRed <= 90) {
+      stroke(0, 0, 0);
+
+    }
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    size(400, 400);
+    stroke(300);
+    stroke(200);
+    fill(intRed, intBlue , intGreen);
+    
+    rect(55, 100, 55, 100);
   
-  // define other methods down here.
+    rect(300, 100, 55, 100);
+
+    triangle(55, 120, 300, 240, 55, 55);
+  
+    triangle(355, 120, 135, 240, 355, 55);
+  
+  }
+
+
+  
 }
